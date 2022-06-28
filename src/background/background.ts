d522842e60148bcd0f13293800f5e6bac2b8b82d
Tyler Turnipseed <tyler@techturnip.us>
2022-06-27 22:00:45 -0400

    add city context menu feature to add selected text

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,4 +1,8 @@
-import { setStoredCities, setStoredOptions } from '../utils/storage'
+import {
+  getStoredCities,
+  setStoredCities,
+  setStoredOptions,
+} from '../utils/storage'
 
 chrome.runtime.onInstalled.addListener(() => {
   setStoredCities([])
@@ -6,5 +10,17 @@ chrome.runtime.onInstalled.addListener(() => {
     hasAutoOverlay: false,
     homeCity: '',
     tempScale: 'imperial',
+  })
+
+  chrome.contextMenus.create({
+    contexts: ['selection'],
+    title: 'Add city to weather extension',
+    id: 'weatherExtension',
+  })
+})
+
+chrome.contextMenus.onClicked.addListener((event) => {
+  getStoredCities().then((cities) => {
+    setStoredCities([...cities, event.selectionText])
   })
 })
